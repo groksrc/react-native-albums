0caf8f35c87450d415e585c20751b6e64e33ec84
Drew Cain <drew@simpli.fi>
2018-07-07 19:59:32 -0500

    Add content to CardSection

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,58 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import Card from './Card'
 import CardSection from './CardSection'
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, image, thumbnail_image } = album
+  const {
+    albumStyle,
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailStyle,
+    thumbnailContainerStyle
+  } = styles
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View style={thumbnailContainerStyle}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image style={albumStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  albumStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
 }
 
 export default AlbumDetail
